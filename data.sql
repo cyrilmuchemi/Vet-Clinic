9aa48e47e838b8d05d4df2fd562936a715d997f8
afikanzoro <muchemimutahi@gmail.com>
2022-08-09 16:35:15 +0300

    Do basic SQL operations

--- data.sql
+++ data.sql
@@ -1,5 +1,7 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES (2, 'Gabumon', '2018-11-15', 2, TRUE, 8.0),
+(3, 'Pikachu', '2021-07-01', 1, FALSE, 15.04),
+(4, 'Devimon', '2017-05-17', 5, TRUE, 11);
+
