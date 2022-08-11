346979ec2e44c7de3ecf49293fae8515bb615897
afikanzoro <muchemimutahi@gmail.com>
2022-08-11 10:49:18 +0300

    Make changes as requested by the reviewer

--- data.sql
+++ data.sql
@@ -1,17 +1,17 @@
 /* Populate database with sample data. */
 
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES (2, 'Gabumon', '2018-11-15', 2, TRUE, 8.0),
-(3, 'Pikachu', '2021-07-01', 1, FALSE, 15.04),
-(4, 'Devimon', '2017-05-17', 5, TRUE, 11);
+VALUES ('Gabumon', '2018-11-15', 2, TRUE, 8.0),
+('Pikachu', '2021-07-01', 1, FALSE, 15.04),
+('Devimon', '2017-05-17', 5, TRUE, 11.0);
 
 
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES(5, 'Charmander', '2020-02-08', 0, FALSE, -11),
-(6, 'Plantmon', '2021-11-15', 2, TRUE, -5.7 ),
-(7, 'Squirtle', '1993-04-02', 3, FALSE, -12.13),
-(8, 'Angemon', '2005-06-12', 1, TRUE, -45 ),
-(9, 'Boarmon', '2005-07-06', 7, TRUE, 20.4),
-(10, 'Blossom', '1998-10-13', 3, TRUE, 17),
-(11, 'Ditto', '2022-05-14', 4, TRUE, 22);
+VALUES('Charmander', '2020-02-08', 0, FALSE, -11.0),
+('Plantmon', '2021-11-15', 2, TRUE, -5.7 ),
+('Squirtle', '1993-04-02', 3, FALSE, -12.13),
+('Angemon', '2005-06-12', 1, TRUE, -45.0 ),
+('Boarmon', '2005-07-06', 7, TRUE, 20.4),
+('Blossom', '1998-10-13', 3, TRUE, 17.0),
+('Ditto', '2022-05-14', 4, TRUE, 22.0);
 
--- queries.sql
+++ queries.sql
@@ -9,6 +9,34 @@ SELECT * from animals WHERE neutered = 't';
 SELECT * from animals WHERE name <> 'Gabumon';
 SELECT * from animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
 
+BEGIN;
+UPDATE animals
+SET species = 'unspecified';
+SELECT * from animals;
+ROLLBACK;
+SELECT * from animals;
+
+BEGIN;
+UPDATE animals SET species = 'digimon'
+WHERE name like '%mon';
+UPDATE animals SET species = 'pokemon'
+WHERE species IS NULL;
+COMMIT;
+
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+SELECT * from animals;
+
+BEGIN;
+DELETE FROM animals
+WHERE date_of_birth > 'January 1, 2022';
+SAVEPOINT SP1;
+UPDATE animals SET weight_kg = (weight_kg * -1);
+ROLLBACK TO SP1;
+UPDATE animals SET weight_kg = (weight_kg * -1) WHERE weight_kg < 0;
+COMMIT;
+
 SELECT COUNT(*) FROM animals;
 SELECT * from animals WHERE  escape_attempts = 0;
 SELECT AVG(weight_kg) FROM animals;
