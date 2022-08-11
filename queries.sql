11765575611842b0e56b409cc48e57edd5b73deb
afikanzoro <muchemimutahi@gmail.com>
2022-08-11 11:13:15 +0300

    Make tweaks

--- queries.sql
+++ queries.sql
@@ -42,3 +42,8 @@ SELECT * from animals WHERE  escape_attempts = 0;
 SELECT AVG(weight_kg) FROM animals;
 SELECT neutered, MAX(escape_attempts) FROM animals GROUP BY neutered;
 SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
+
+SELECT species, AVG(escape_attempts)
+FROM animals
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
+GROUP BY species;
