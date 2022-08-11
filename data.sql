9d3631a3da9ce708914de90094c8cc4b5d6e968f
afikanzoro <muchemimutahi@gmail.com>
2022-08-11 18:13:28 +0300

    Create owners & species tables

--- data.sql
+++ data.sql
@@ -1,9 +1,10 @@
 /* Populate database with sample data. */
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES ('Gabumon', '2018-11-15', 2, TRUE, 8.0),
-('Pikachu', '2021-07-01', 1, FALSE, 15.04),
-('Devimon', '2017-05-17', 5, TRUE, 11.0);
+VALUES  ('Agumon', 'February 3, 2020', 0, TRUE, 10.23),
+    ('Gabumon', 'November 15, 2018', 2, TRUE, 8),
+    ('Pikachu', 'January 7, 2021', 1, FALSE, 15.04),
+    ('Devimon', 'May 12, 2017', 5, TRUE, 11);
 
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
@@ -14,4 +15,90 @@ VALUES('Charmander', '2020-02-08', 0, FALSE, -11.0),
 ('Boarmon', '2005-07-06', 7, TRUE, 20.4),
 ('Blossom', '1998-10-13', 3, TRUE, 17.0),
 ('Ditto', '2022-05-14', 4, TRUE, 22.0);
+
+INSERT INTO
+    owners(full_name, age)
+VALUES
+    ('Sam Smith', 34),
+    ('Jennifer Orwell', 19),
+    ('Bob', 45),
+    ('Melody Pond', 77),
+    ('Dean Winchester', 14),
+    ('Jodie Whittaker', 38);
+
+INSERT INTO
+    species(name)
+VALUES
+    ('Pokemon'),
+    ('Digimon');
+
+UPDATE
+    animals
+SET
+    species_id = species.id
+FROM
+    species
+WHERE
+    animals.name like '%mon'
+    AND species.name = 'Digimon';
+
+UPDATE
+    animals
+SET
+    species_id = species.id
+FROM
+    species
+WHERE
+    animals.name NOT like '%mon'
+    AND species.name = 'Pokemon';
+
+UPDATE
+    animals
+SET
+    owner_id = owners.id
+FROM
+    owners
+WHERE
+    animals.name = 'Agumon'
+    AND owners.full_name = 'Sam Smith';
+
+UPDATE
+    animals
+SET
+    owner_id = owners.id
+FROM
+    owners
+WHERE
+    animals.name IN ('Gabumon', 'Pikachu')
+    AND owners.full_name = 'Jennifer Orwell';
+
+UPDATE
+    animals
+SET
+    owner_id = owners.id
+FROM
+    owners
+WHERE
+    animals.name IN ('Devimon', 'Plantmon')
+    AND owners.full_name = 'Bob';
+
+UPDATE
+    animals
+SET
+    owner_id = owners.id
+FROM
+    owners
+WHERE
+    animals.name IN ('Charmander', 'Squirtle', 'Blossom')
+    AND owners.full_name = 'Melody Pond';
+
+UPDATE
+    animals
+SET
+    owner_id = owners.id
+FROM
+    owners
+WHERE
+    animals.name IN ('Angemon', 'Boarmon')
+    AND owners.full_name = 'Dean Winchester';
 
--- queries.sql
+++ queries.sql
@@ -47,3 +47,71 @@ SELECT species, AVG(escape_attempts)
 FROM animals
 WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
 GROUP BY species;
+
+SELECT
+    name
+FROM
+    animals
+    INNER JOIN owners ON animals.owner_id = owners.id
+WHERE
+    owners.full_name = 'Melody Pond';
+
+SELECT
+    animals.name
+FROM
+    animals
+    INNER JOIN species ON animals.species_id = species.id
+WHERE
+    species.name = 'Pokemon';
+
+SELECT
+    full_name,
+    name
+FROM
+    owners
+    LEFT JOIN animals ON owners.id = animals.owner_id;
+
+SELECT
+    species.name as species_name,
+    count(*) as number_of_animals
+FROM
+    animals
+    INNER JOIN species ON species.id = animals.species_id
+GROUP BY
+    species.name;
+
+SELECT
+    owners.full_name as owner_name,
+    animals.name as animal_name,
+    species.name as species_name
+FROM
+    animals
+    INNER JOIN species ON species.id = animals.species_id
+    INNER JOIN owners ON owners.id = animals.owner_id
+WHERE
+    owners.full_name = 'Jennifer Orwell'
+    AND species.name = 'Digimon';
+
+SELECT
+    owners.full_name as owner_name,
+    animals.name as animal_name,
+    animals.escape_attempts
+FROM
+    animals
+    INNER JOIN owners ON owners.id = animals.owner_id
+WHERE
+    owners.full_name = 'Dean Winchester'
+    AND animals.escape_attempts = 0;
+
+SELECT
+    owners.full_name as most_animals_owner,
+    count(*) as animals_owned
+FROM
+    owners
+    INNER JOIN animals ON owners.id = animals.owner_id
+GROUP BY
+    owners.full_name
+ORDER BY
+    animals_owned DESC
+LIMIT
+    1;
